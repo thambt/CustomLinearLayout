0accf9394bdc03620790c8627f70e6a7cba7726e
Thambt <thambui2503@gmail.com>
2017-06-29 18:23:13 +0700

    add animation.

--- CustomLayoutW3/app/src/main/java/com/example/tham/customlayoutw3/CustomLinearLAyout.java
+++ CustomLayoutW3/app/src/main/java/com/example/tham/customlayoutw3/CustomLinearLAyout.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.Rect;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -20,8 +21,7 @@ import android.widget.TextView;
  */
 
 public class CustomLinearLAyout extends LinearLayout {
-
-
+    Boolean flagAnimation ;
     TextView view1;
     TextView view2;
     TextView view3;
@@ -63,8 +63,10 @@ public class CustomLinearLAyout extends LinearLayout {
                     R.styleable.MyCheckBox,
                     0, 0);
             int color1 = Color.BLUE;
+            int myGravity = 3;
             try {
                 color1 = a.getInt(R.styleable.MyCheckBox_background_view,Color.BLUE);
+                myGravity = a.getInt(R.styleable.MyCheckBox_mygravity,3);
 
             } catch (Exception e) {
                 Log.e("MyCheckBox", "There was an error loading attributes.");
@@ -72,38 +74,61 @@ public class CustomLinearLAyout extends LinearLayout {
                 a.recycle();
             }
             setColorView(color1);
+            setMyGravity(myGravity);
         }
     }
 
     public void changeLayout() {
         if(llParaentLayout.getOrientation() == LinearLayout.HORIZONTAL) {
-            view1.setAnimation(initMoveAnimation(0));
-            llParaentLayout.setOrientation(LinearLayout.VERTICAL);
-            view1.setWidth(1000);
-            view1.setHeight(100);
-            view2.setWidth(1000);
-            view2.setHeight(100);
-            view3.setWidth(1000);
-            view3.setHeight(100);
-            view4.setWidth(1000);
-            view4.setHeight(100);
-
+            changeVertacal();
         } else {
-            llParaentLayout.setOrientation(LinearLayout.HORIZONTAL);
-            view1.setHeight(1000);
-            view1.setWidth(100);
-            view2.setHeight(1000);
-            view2.setWidth(100);
-            view3.setHeight(1000);
-            view3.setWidth(100);
-            view4.setHeight(1000);
-            view4.setWidth(100);
+            changeHorizontal();
         }
     }
 
+    private void changeHorizontal() {
+        view1.setAnimation(initAppearAnimation(0));
+        view2.setAnimation(initAppearAnimation(2000));
+        view3.setAnimation(initAppearAnimation(4000));
+        view4.setAnimation(initAppearAnimation(6000));
+        llParaentLayout.setOrientation(LinearLayout.HORIZONTAL);
+        view1.setHeight(1000);
+        view1.setWidth(100);
+        view2.setHeight(1000);
+        view2.setWidth(100);
+        view3.setHeight(1000);
+        view3.setWidth(100);
+        view4.setHeight(1000);
+        view4.setWidth(100);
+    }
+
+    private void changeVertacal() {
+        view1.setAnimation(initAppearAnimation(0));
+        view2.setAnimation(initAppearAnimation(2000));
+        view3.setAnimation(initAppearAnimation(4000));
+        view4.setAnimation(initAppearAnimation(6000));
+        llParaentLayout.setOrientation(LinearLayout.VERTICAL);
+        view1.setHeight(100);
+        view1.setWidth(1000);
+        view2.setHeight(100);
+        view2.setWidth(1000);
+        view3.setHeight(100);
+        view3.setWidth(1000);
+        view4.setHeight(100);
+        view4.setWidth(1000);
+    }
+
     private Animation initMoveAnimation(long delay) {
-        TranslateAnimation animation = new TranslateAnimation(0,1000,0,0);
-        animation.setDuration(3000);
+        TranslateAnimation animation = new TranslateAnimation(1000,0,0,0);
+        animation.setDuration(2000);
+        animation.setFillAfter(false);
+        animation.setStartOffset(delay);
+        return animation;
+    }
+
+    private Animation initAppearAnimation(long delay) {
+        TranslateAnimation animation = new TranslateAnimation(1000,0,0,0);
+        animation.setDuration(2000);
         animation.setFillAfter(false);
         animation.setStartOffset(delay);
         return animation;
@@ -114,6 +139,10 @@ public class CustomLinearLAyout extends LinearLayout {
         view2.setBackgroundColor(color1);
         view3.setBackgroundColor(color1);
         view4.setBackgroundColor(color1);
+    }
+
+    private void setMyGravity(int gravity) {
+        ((LinearLayout.LayoutParams) llParaentLayout.getLayoutParams()).gravity = gravity;
     }
 
     public CustomLinearLAyout(Context context, AttributeSet attrs, int defStyle) {
@@ -140,6 +169,4 @@ public class CustomLinearLAyout extends LinearLayout {
             }
         });
     }
-
-
 }
--- CustomLayoutW3/app/src/main/java/com/example/tham/customlayoutw3/MainActivity.java
+++ CustomLayoutW3/app/src/main/java/com/example/tham/customlayoutw3/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.tham.customlayoutw3;
 
+import android.os.Handler;
 import android.support.annotation.BoolRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -12,6 +15,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private Button btnChange;
     private CustomLinearLAyout myLayout;
+    private Animation.AnimationListener animationListener;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +24,23 @@ public class MainActivity extends AppCompatActivity {
         btnChange = (Button)findViewById(R.id.activity_main_btn_change);
         myLayout = (CustomLinearLAyout)findViewById(R.id.activity_main_myll);
 
+        final Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.anim_move_x);
+        animation.setAnimationListener(animationListener);
+
         btnChange.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myLayout.changeLayout();
+                myLayout.view1.startAnimation(animation);
+                myLayout.view2.startAnimation(animation);
+                myLayout.view3.startAnimation(animation);
+                myLayout.view4.startAnimation(animation);
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        myLayout.changeLayout();
+                    }
+                }, 2000);
             }
         });
     }
